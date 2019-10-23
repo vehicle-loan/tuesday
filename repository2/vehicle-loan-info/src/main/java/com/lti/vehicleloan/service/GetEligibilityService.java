5cdf7cb365938a491f5b2d84dd12ae4fe8479592
vehicle-loan <56189805+vehicle-loan@users.noreply.github.com>
2019-10-23 19:55:52 +0530

    Add files via upload

---
+++ repository2/vehicle-loan-info/src/main/java/com/lti/vehicleloan/service/GetEligibilityService.java
@@ -0,0 +1,13 @@
+package com.lti.vehicleloan.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class GetEligibilityService{
+	
+	public boolean verify() {
+		return true;
+	}
+	
+}
